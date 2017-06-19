5bd1412dd51f3f00e06fa11c155fb070ccf28850
Andreas C. Osowski <andreas@osowski.de>
2017-06-19 23:23:51 +0200

    add basic curl implementation testsuite

--- curl/src/curl.rs
+++ curl/src/curl.rs
@@ -2,8 +2,14 @@ use trytes::Trinary;
 
 /// All implementations of `Curl` must implement this trait.
 pub trait Curl {
-   /// Absorb a `Trinary` into the sponge
-   fn absorb(&mut self, trinary: Trinary);
-   /// Squeeze the sponge and return a `Trinary` with `tritCount` trits
-   fn squeeze(&mut self, trit_count: usize) -> Trinary;
+    /// Absorb a `Trinary` into the sponge
+    fn absorb(&mut self, trinary: Trinary);
+    /// Squeeze the sponge and return a `Trinary` with `tritCount` trits
+    fn squeeze(&mut self, trit_count: usize) -> Trinary;
+}
+
+/// An implementation of `Curl` that can reset its internal state.
+pub trait CurlWithReset: Curl {
+    /// Resets the sponge's internal state.
+    fn reset(&mut self);
 }
--- curl/src/lib.rs
+++ curl/src/lib.rs
@@ -3,7 +3,7 @@ extern crate iota_trytes as trytes;
 pub mod constants;
 pub mod curl;
 pub mod simple;
-pub mod tests;
+mod tests;
 
 pub use tests::testsuite;
 pub use curl::*;
