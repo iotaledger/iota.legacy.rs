f32c4562cbe9a620f3f527a04744475625225b83
Andreas C. Osowski <andreas@osowski.de>
2017-06-21 23:32:01 +0200

    sign: Implement ISS scheme

---
+++ sign/src/iss.rs
@@ -0,0 +1,149 @@
+// This is a straight clone off https://github.com/Come-from-Beyond/ISS/blob/master/src/cfb/iss/ISS.java for testing purposes.
+// XXX DO NOT EXPECT THE METHODS OR CODE IN THIS MODULE TO PERSIST XXX
+use trytes::*;
+use trytes::constants::RADIX;
+use curl::*;
+
+const TRYTE_WIDTH: usize = 3;
+const MAX_TRYTE_VALUE: i8 = 1;
+const MIN_TRYTE_VALUE: i8 = -1;
+const KEY_LENGTH: usize = ((HASH_LENGTH / 3) / RADIX as usize) * HASH_LENGTH;
+const DIGEST_LENGTH: usize = HASH_LENGTH;
+const ADDRESS_LENGTH: usize = HASH_LENGTH;
+const BUNDLE_LENGTH: usize = HASH_LENGTH;
+const SIGNATURE_LENGTH: usize = KEY_LENGTH;
+
+pub fn subseed<C>(seed: Trinary, mut index: usize) -> Trinary
+where
+    C: Curl + Default,
+{
+    let mut trits = seed.trits();
+    let mut curl = C::default();
+
+    while index > 0 {
+        for i in 0..trits.len() {
+            trits[i] += 1;
+
+            if trits[i] > 1 {
+                trits[i] = -1;
+            } else {
+                break;
+            }
+        }
+
+        index -= 1;
+    }
+
+    curl.absorb(&trits);
+    curl.squeeze(trits.len()).into_iter().collect()
+}
+
+pub fn key<C>(subseed: Trinary) -> Trinary
+where
+    C: Curl + Default,
+{
+    let mut c = C::default();
+    let trits = subseed.trits();
+    c.absorb(&trits);
+    let mut key = c.squeeze(KEY_LENGTH);
+
+    for offset in 0..KEY_LENGTH {
+        c.reset();
+        c.absorb(&key[offset..HASH_LENGTH]);
+        key[offset..HASH_LENGTH].clone_from_slice(c.squeeze(HASH_LENGTH).as_slice());
+    }
+
+    key.into_iter().collect()
+}
+
+pub fn digest_key<C>(key: Trinary) -> Trinary
+where
+    C: Curl + Default,
+{
+    let mut digestCurl = C::default();
+    let mut keyFragmentCurl = C::default();
+    let trits = key.trits();
+
+    for i in 0..(KEY_LENGTH / HASH_LENGTH) {
+        let mut buffer: Vec<Trit> = trits[i * HASH_LENGTH..(i + 1) * HASH_LENGTH]
+            .iter()
+            .cloned()
+            .collect();
+
+        for j in 0..(MAX_TRYTE_VALUE - MIN_TRYTE_VALUE) {
+            keyFragmentCurl.reset();
+            keyFragmentCurl.absorb(&buffer);
+            buffer.clone_from_slice(&keyFragmentCurl.squeeze(HASH_LENGTH));
+        }
+
+        digestCurl.absorb(&buffer);
+    }
+
+    digestCurl.squeeze(DIGEST_LENGTH).into_iter().collect()
+}
+
+pub fn address<C>(digests: Trinary) -> Trinary
+where
+    C: Curl + Default,
+{
+    let mut c = C::default();
+    let trits = digests.trits();
+    c.absorb(&trits);
+    c.squeeze(ADDRESS_LENGTH).into_iter().collect()
+}
+
+pub fn signature<C>(bundle: Trinary, key: Trinary) -> Trinary
+where
+    C: Curl + Default,
+{
+
+    let mut c = C::default();
+
+    let mut signature = key.trits();
+    let bundleTrits = bundle.trits();
+
+    for i in 0..(SIGNATURE_LENGTH / HASH_LENGTH) {
+        let hashingChainLength = MAX_TRYTE_VALUE -
+            (bundleTrits[i * TRYTE_WIDTH] + bundleTrits[i * TRYTE_WIDTH + 1] * 3 +
+                 bundleTrits[i * TRYTE_WIDTH + 2] * 9);
+        for j in hashingChainLength..0 {
+            c.reset();
+            c.absorb(&signature[i * HASH_LENGTH..(i + 1) * HASH_LENGTH]);
+            signature[i * HASH_LENGTH..(i + 1) * HASH_LENGTH]
+                .clone_from_slice(c.squeeze(HASH_LENGTH).as_slice());
+        }
+    }
+
+    signature.into_iter().collect()
+}
+
+pub fn digest_bundle_signature<C>(bundle: Trinary, signature: Trinary) -> Trinary
+where
+    C: Curl + Default,
+{
+    let mut digestCurl = C::default();
+    let mut signatureFragmentCurl = C::default();
+
+    let signatureTrits = signature.trits();
+    let bundleTrits = bundle.trits();
+
+    for i in 0..(SIGNATURE_LENGTH / HASH_LENGTH) {
+        let hashingChainLength = MAX_TRYTE_VALUE -
+            (bundleTrits[i * TRYTE_WIDTH] + bundleTrits[i * TRYTE_WIDTH + 1] * 3 +
+                 bundleTrits[i * TRYTE_WIDTH + 2] * 9);
+
+        let mut buffer: Vec<Trit> = signatureTrits[i * HASH_LENGTH..(i + 1) * HASH_LENGTH]
+            .iter()
+            .cloned()
+            .collect();
+        for j in hashingChainLength..0 {
+            signatureFragmentCurl.reset();
+            signatureFragmentCurl.absorb(&signatureTrits[i * HASH_LENGTH..(i + 1) * HASH_LENGTH]);
+            buffer.clone_from_slice(signatureFragmentCurl.squeeze(HASH_LENGTH).as_slice());
+        }
+
+        digestCurl.absorb(&buffer);
+    }
+
+    digestCurl.squeeze(DIGEST_LENGTH).into_iter().collect()
+}
