7df6050c10bcbb1d9b944fc2e02581f12bbbc651
Paul D Handy <paul.d.handy@gmail.com>
2017-06-24 09:56:11 -0600

    exposed BCTrit type

--- trytes/src/lib.rs
+++ trytes/src/lib.rs
@@ -14,10 +14,10 @@ pub mod bct;
 
 pub use constants::TRYTE_ALPHABET;
 pub use constants::Trit;
+pub use constants::BCTrit;
 pub use constants::HASH_LENGTH;
 
 pub use trinary::*;
 pub use string::*;
 pub use trits::*;
 pub use iter::*;
-
