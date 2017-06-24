65638432ddecddf31c81518b3c77d947f3bacff8
Paul D Handy <paul.d.handy@gmail.com>
2017-06-24 09:49:51 -0600

    added multi-curl

--- curl/src/curl.rs
+++ curl/src/curl.rs
@@ -1,14 +1,69 @@
-use trytes::Trit;
+use constants::*;
+//use trytes::Trit;
+use trytes::*;
 use trytes::Trinary;
 
 /// All implementations of `Curl` must implement this trait.
-pub trait Curl {
-    /// Absorb a `&[Trit]` into the sponge
-    fn absorb(&mut self, trits: &[Trit]);
-    /// Squeeze the sponge and return a `Trinary` with `tritCount` trits
-    fn squeeze(&mut self, trit_count: usize) -> Vec<Trit>;
-    /// Transforms the sponge
+
+pub trait Sponge {
     fn transform(&mut self);
-    /// Resets the sponge's internal state.
     fn reset(&mut self);
+}
+
+#[derive(Copy)]
+pub struct Curl<T>
+    where T: Clone + Copy + Sized
+{
+    pub state: [T; STATE_LENGTH],
+}
+
+impl<T> Clone for Curl<T>
+    where T: Clone + Copy + Sized
+{
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+
+impl<T> Curl<T>
+    where T: Clone + Copy + Sized,
+          Curl<T>: Sponge
+{
+    fn absorb(&mut self, trits: &[T], offset_in: usize, length_in: usize) {
+        let mut len = length_in;
+        let mut offset = offset_in;
+        loop {
+            let to = if len < HASH_LENGTH { len } else { HASH_LENGTH };
+            self.state[0..to].clone_from_slice(&trits[offset..offset + to]);
+
+            Sponge::transform(self);
+
+            offset += HASH_LENGTH;
+            if len <= HASH_LENGTH {
+                break;
+            }
+            len -= HASH_LENGTH;
+        }
+    }
+
+    fn squeeze(&mut self, offset_in: usize, trit_count: usize) -> Vec<T> {
+        let mut len = trit_count;
+        let mut out: Vec<T> = Vec::with_capacity(trit_count);
+        let mut offset = offset_in;
+
+        loop {
+            let to = if len < HASH_LENGTH { len } else { HASH_LENGTH };
+            out.extend_from_slice(&self.state[0..to]);
+            self.transform();
+
+            offset += HASH_LENGTH;
+            if len <= HASH_LENGTH {
+                break;
+            }
+            len -= HASH_LENGTH;
+        }
+
+        out
+    }
 }
---
+++ curl/src/pair.rs
@@ -0,0 +1,58 @@
+use rayon::prelude::*;
+use constants::*;
+use trytes::*;
+use curl::*;
+use indices::*;
+//use std::mem;
+
+fn step(first: BCTrit, second: BCTrit) -> BCTrit {
+    let (alpha, beta) = first;
+    let (delta_0, gamma) = second;
+    let delta = (alpha | (!gamma)) & (delta_0 ^ beta);
+    (!delta, (alpha ^ gamma) | delta)
+}
+
+/// Tuple implementation of the `Sponge` for Curl
+impl Sponge for Curl<BCTrit> {
+    fn transform(&mut self) {
+        // Required memory space type for computation
+        //let mut scratchpad: Vec<BCTrit> = self.state.clone();
+        let STATE_INDICES: Vec<_> = (0..STATE_LENGTH).collect();
+        let mut scratchpad: Vec<BCTrit> = self.state.iter().map(|&c| (c.0, c.1)).collect();
+
+        for _ in 0..NUMBER_OF_ROUNDS {
+            scratchpad = STATE_INDICES
+                .par_iter()
+                .map(|&i| {
+                         step(scratchpad[TRANSFORM_INDICES[i]],
+                              scratchpad[TRANSFORM_INDICES[i + 1]])
+                     })
+                .collect();
+        }
+        self.state.clone_from_slice(&scratchpad);
+    }
+
+    fn reset(&mut self) {
+        self.state = [(0, 0); STATE_LENGTH];
+    }
+}
+
+impl Default for Curl<BCTrit> {
+    fn default() -> Self {
+        let x: BCTrit = (0, 0);
+        Curl::<BCTrit> { state: [x; STATE_LENGTH] }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn run_testsuite() {
+        use tests::testsuite;
+        testsuite::run::<Curl<BCTrit>>();
+    }
+
+}
