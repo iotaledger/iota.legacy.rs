b78001cc454a35ba857c3abdd5b579f9fb416cdb
Andreas C. Osowski <andreas@osowski.de>
2017-06-25 11:40:48 +0200

    trytes: multiplex mod, comments, tests

---
+++ trytes/src/multiplex/constants.rs
@@ -0,0 +1,8 @@
+#[cfg(target_pointer_width = "16")]
+pub const MAX_TRINARIES: usize = 16;
+#[cfg(target_pointer_width = "32")]
+pub const MAX_TRINARIES: usize = 32;
+#[cfg(target_pointer_width = "64")]
+pub const MAX_TRINARIES: usize = 64;
+#[cfg(target_pointer_width = "128")]
+pub const MAX_TRINARIES: usize = 128;
