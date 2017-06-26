14ceccd683baa7850a10d75a7cc32c269d98ea13
Andreas C. Osowski <andreas@osowski.de>
2017-06-26 10:01:52 +0200

    move iota.rs.js to bindings

---
+++ bindings/src/curl/simple.rs
@@ -0,0 +1,47 @@
+use iota_trytes::*;
+use iota_curl::*;
+use cty::*;
+use alloc::Vec;
+use alloc::string::ToString;
+use core::mem;
+use alloc::boxed::Box;
+
+use util::c_str_to_static_slice;
+
+#[no_mangle]
+pub fn curl_simple_new() -> *mut c_void {
+    let curl = Box::new(Curl::<Trit>::default());
+    Box::into_raw(curl) as *mut c_void
+}
+
+#[no_mangle]
+pub fn curl_simple_delete(c_curl: *mut c_void) {
+    unsafe { mem::drop(Box::from_raw(c_curl)) }
+}
+
+#[no_mangle]
+pub fn curl_simple_absorb(c_curl: *mut c_void, trinary: *const c_char) {
+    let trinary_str = unsafe { c_str_to_static_slice(trinary) };
+    let trinary: Trinary = trinary_str.chars().collect();
+
+    let curl: &mut Curl<Trit>= unsafe { &mut *(c_curl as *mut Curl<Trit>) };
+    let trits: Vec<Trit> = trinary.trits();
+    curl.absorb(trits.as_slice());
+}
+
+#[no_mangle]
+pub fn curl_simple_reset(c_curl: *mut c_void) {
+    let mut curl: Box<Curl<Trit>> = unsafe { Box::from_raw(c_curl as *mut Curl<Trit>) };
+    curl.reset();
+}
+
+#[no_mangle]
+pub fn curl_simple_squeeze(c_curl: *mut c_void, trit_count: isize) -> *const u8 {
+    let mut curl: Box<Curl<Trit>> = unsafe { Box::from_raw(c_curl as *mut Curl<Trit>) };
+    let trits = curl.squeeze(trit_count as usize);
+
+    let trinary: Trinary = trits.into_iter().collect();
+    let trinary_str = Box::new(trinary.to_string() + "\0");
+
+    &trinary_str.as_bytes()[0] as *const u8
+}
---
+++ bindings/src/lib.rs
@@ -0,0 +1,31 @@
+#![feature(start)]
+#![feature(alloc)]
+#![feature(lang_items)]
+#![feature(link_args)]
+#![no_std]
+#![crate_type = "staticlib"]
+
+#![cfg(not(test))]
+#![feature(core_intrinsics)]
+
+extern crate alloc;
+extern crate cty;
+
+extern crate iota_sign;
+extern crate iota_trytes;
+extern crate iota_curl;
+
+#[cfg(not(test))] 
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[cfg(not(test))] 
+#[lang = "panic_fmt"]
+fn panic_fmt() -> ! {
+    use core::intrinsics;
+    unsafe {intrinsics::abort(); }
+}
+
+pub mod util;
+pub mod sign;
+pub mod curl;
