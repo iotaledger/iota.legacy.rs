6304d96e2b04d071faf6b2241274e2dc86dd76f9
Andreas C. Osowski <andreas@osowski.de>
2017-06-26 20:38:13 +0200

    curl: fix debug build

--- curl/src/simple.rs
+++ curl/src/simple.rs
@@ -9,36 +9,16 @@ use indices::*;
 const TRUTH_TABLE: [Trit; 11] = [1, 0, -1, 2, 1, -1, 0, 2, -1, 1, 0];
 
 impl Sponge for Curl<Trit> {
-    #[cfg(feature = "parallel")]
-    fn transform(&mut self) {
-        use collections::Vec;
-
-        let mut scratchpad: Vec<Trit> = self.state.iter().cloned().collect();
-
-        for _ in 0..NUMBER_OF_ROUNDS {
-            scratchpad = (0..STATE_LENGTH)
-                .into_par_iter()
-                .map(|i| {
-                    let idx: usize = (self.state[TRANSFORM_INDICES[i]] as usize) +
-                        ((self.state[TRANSFORM_INDICES[i + 1]] as usize) << 2) +
-                        5;
-                    TRUTH_TABLE[idx]
-                })
-                .collect();
-        }
-
-        self.state.copy_from_slice(&scratchpad);
-    }
-
-    #[cfg(not(feature = "parallel"))]
     fn transform(&mut self) {
         let mut state_clone: [Trit; STATE_LENGTH] = [0; STATE_LENGTH];
 
         for _ in 0..NUMBER_OF_ROUNDS {
             for state_index in 0..STATE_LENGTH {
-                let idx: usize = self.state[TRANSFORM_INDICES[state_index]] as usize +
-                    ((self.state[TRANSFORM_INDICES[state_index + 1]] as usize) << 2) +
-                    5;
+                let idx: usize = (self.state[TRANSFORM_INDICES[state_index]] as usize)
+                    .wrapping_add(
+                        (self.state[TRANSFORM_INDICES[state_index + 1]] as usize) << 2,
+                    )
+                    .wrapping_add(5);
 
                 state_clone[state_index] = TRUTH_TABLE[idx];
             }
