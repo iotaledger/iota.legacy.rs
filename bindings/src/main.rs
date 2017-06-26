6f652ac30758d835f9f6b6cbec67787e904ed880
Andreas C. Osowski <andreas@osowski.de>
2017-06-26 13:26:32 +0200

    bindings: support output to wasm curl: add simple benchmarks

--- bindings/src/main.rs
+++ bindings/src/main.rs
@@ -19,7 +19,7 @@ pub mod util;
 pub mod sign;
 pub mod curl;
 
-#[cfg(target_os = "emscripten")]
+#[cfg(any(target_os = "emscripten", target_arch="wasm32"))]
 #[link_args = "-s EXPORTED_FUNCTIONS=['_curl_pair_new','_curl_pair_absorb','_curl_pair_squeeze','_curl_pair_reset','_curl_pair_delete'\
 ,'_curl_simple_new','_curl_simple_absorb','_curl_simple_squeeze','_curl_simple_reset','_curl_simple_delete'\
 ,'_subseed','_key','_digest_key','_address','_signature','_digest_bundle_signature'\
--- curl/src/lib.rs
+++ curl/src/lib.rs
@@ -1,6 +1,11 @@
-#![no_std]
+#![feature(test)]
 #![feature(alloc)]
+#![no_std]
 extern crate alloc as collections;
+
+
+#[cfg(test)]
+extern crate test;
 
 #[cfg(feature = "parallel")]
 extern crate rayon;
@@ -12,6 +17,9 @@ pub mod curl;
 pub mod simple;
 pub mod pair;
 mod tests;
+
+#[cfg(test)]
+mod bench;
 
 pub use tests::testsuite;
 pub use curl::*;
