af48cdb9e5793a9cedbab6d3d69fd931a00e0221
Andreas C. Osowski <andreas@osowski.de>
2017-06-30 16:09:47 +0200

    sign: add Checksum and FromTrinaryWithChecksum traits and stub impl

--- sign/src/lib.rs
+++ sign/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
-
+#![feature(fixed_size_array)]
 #![feature(alloc)]
+
 extern crate alloc;
 
 extern crate iota_trytes as trytes;
@@ -8,3 +9,6 @@ extern crate iota_curl as curl;
 extern crate iota_curl_cpu as curl_cpu;
 
 pub mod iss;
+pub mod checksum;
+
+pub use checksum::*;
