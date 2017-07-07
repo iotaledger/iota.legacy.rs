cc76f2514d368a72b51e49a19e1122f0722e18ed
Paul D Handy <paul.d.handy@gmail.com>
2017-07-06 22:47:53 -0600

    removed atomicbool arc and comment

--- curl-cpu/src/search.rs
+++ curl-cpu/src/search.rs
@@ -47,10 +47,9 @@ mod cpu_search {
     where
         F: Fn(&[BCTrit]) -> Option<usize> + 'static + Send + Sync,
     {
-        let running = Arc::new(AtomicBool::new(true));
+        let running = AtomicBool::new(true);
         let check_arc = Arc::new(check);
         let (tx, rx) = channel();
-        // TODO: replace with calculated number of threads
         let handles: Vec<thread::JoinHandle<_>> = (0..8)
             .into_iter()
             .map(|i| {
@@ -59,14 +58,13 @@ mod cpu_search {
                 let mut size = min(length, HASH_LENGTH);
                 let child_tx = tx.clone();
                 let child_group = i + group;
-                let running_clone = running.clone();
                 let check_clone = check_arc.clone();
                 thread::spawn(move || {
                     for _ in 0..child_group {
                         (&mut curl.state[(size / 3)..(size * 2 / 3)]).incr();
                     }
                     let mut index: Option<usize> = None;
-                    while index.is_none() && running_clone.load(Ordering::SeqCst) {
+                    while index.is_none() && running.load(Ordering::SeqCst) {
                         size = min(
                             size * 2 / 3 + (&mut curl.state[(size * 2 / 3)..size]).incr(),
                             HASH_LENGTH,
@@ -75,8 +73,8 @@ mod cpu_search {
                         cpy.transform();
                         index = check_clone(&cpy.state[..HASH_LENGTH]);
                     }
-                    if running_clone.load(Ordering::SeqCst) && index.is_some() {
-                        running_clone.store(false, Ordering::SeqCst);
+                    if running.load(Ordering::SeqCst) && index.is_some() {
+                        running.store(false, Ordering::SeqCst);
                         let mux = TrinaryDemultiplexer::new(curl.squeeze(size).as_slice());
                         child_tx.send(Some(mux[index.unwrap()].clone())).unwrap();
                     }
