46f4ab1ad17c10b2b83a24d0001c41c8edc8b6ff
Andreas C. Osowski <andreas@osowski.de>
2017-07-19 15:59:05 +0200

    models: add display & debug impl for Nonce(View)

--- models/src/v2/nonce.rs
+++ models/src/v2/nonce.rs
@@ -1,12 +1,13 @@
 use trytes::*;
 use alloc::Vec;
+use core::fmt;
 
 pub const NONCE_LEN_TRITS: usize = 243;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct Nonce(Vec<Trit>);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct NonceView<'a>(&'a [Trit]);
 
 #[derive(Debug, Eq, PartialEq)]
@@ -32,7 +33,7 @@ impl FromTrits<Trit> for Nonce {
 
 impl Nonce {
     pub fn view(&self) -> NonceView {
-       NonceView(self.0.as_slice()) 
+        NonceView(self.0.as_slice())
     }
 }
 
@@ -42,6 +43,35 @@ impl Default for Nonce {
         Nonce(iter::repeat(0).take(NONCE_LEN_TRITS).collect())
     }
 }
+impl<'a> fmt::Display for NonceView<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = trits_to_string(self.0).unwrap();
+        f.write_str(&s)
+    }
+}
+
+impl fmt::Display for Nonce {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.view().fmt(f)
+    }
+}
+
+impl fmt::Debug for Nonce {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Nonce(")
+            .and_then(|_| fmt::Display::fmt(self, f))
+            .and_then(|_| f.write_str(")"))
+    }
+}
+
+impl<'a> fmt::Debug for NonceView<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("NonceView(")
+            .and_then(|_| fmt::Display::fmt(self, f))
+            .and_then(|_| f.write_str(")"))
+    }
+}
+
 
 impl<'a> NonceView<'a> {
     pub fn from_trits(base: &'a [Trit]) -> Result<Self, NonceParseError> {
