ddb7b9fb9a75fc05eb6e7cf13e88b006d3341062
Paul D Handy <paul.d.handy@gmail.com>
2017-08-14 07:22:06 -0600

    absorb-search: added copy

---
+++ curl-cpu/src/copy.rs
@@ -0,0 +1,11 @@
+use curl::*;
+use tmath::*;
+use trytes::*;
+
+#[inline]
+pub fn search_prepare_trits<C: Curl<Trit>, CB: Curl<BCTrit>>(c: &C, cb: &mut CB) {
+    for (b, &t) in cb.state_mut().iter_mut().zip(c.state().iter()) {
+        *b = trit_to_bct(t);
+    }
+    cb.state_mut().offset(0);
+}
