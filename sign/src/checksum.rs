f0323ff749d4d74c725ef3e8ccfe852a3f662bed
Andreas C. Osowski <andreas@osowski.de>
2017-08-14 23:45:21 +0200

    sign: update checksum to use kerl

--- sign/src/checksum.rs
+++ sign/src/checksum.rs
@@ -1,27 +1,30 @@
 use curl::*;
+use kerl::Kerl;
 use trytes::TRITS_PER_TRYTE;
+use trytes::HASH_LENGTH;
+use trytes::Trit;
 
 pub const CHECKSUM_TRYTES : usize = 9;
 pub const CHECKSUM_LEN: usize = CHECKSUM_TRYTES * TRITS_PER_TRYTE;
 
-pub fn trits_checksum<'a, T, C>(t: &'a [T], out: &mut [T], curl: &mut C)
-where
-    T: Clone + Copy + Sized,
-    C: Curl<T>,
+pub fn trits_checksum(t: &[Trit], out: &mut [Trit], kerl: &mut Kerl)
 {
     assert_eq!(out.len(), CHECKSUM_LEN);
-    curl.absorb(t);
-    curl.squeeze(out)
+
+    let mut trits = [0 as Trit; HASH_LENGTH];
+
+    kerl.reset();
+    kerl.absorb(t);
+    kerl.squeeze(&mut trits);
+
+    out.clone_from_slice(&trits[HASH_LENGTH-CHECKSUM_LEN..HASH_LENGTH]);
 }
 
 pub fn trits_without_checksum<'a, T>(t: &'a [T]) -> &'a [T] {
     &t[0..t.len() - CHECKSUM_LEN]
 }
 
-pub fn trits_validate_checksum<'a, T, C>(t: &'a [T], curl: &mut C) -> Option<ChecksumValidationError>
-where
-    T: Clone + Copy + Sized + PartialEq,
-    C: Curl<T>
+pub fn trits_validate_checksum(t: &[Trit], kerl: &mut Kerl) -> Option<ChecksumValidationError>
 {
     use ChecksumValidationError::*;
 
@@ -29,11 +32,10 @@ where
         return Some(InvalidLength);
     }
 
-
     let (body, rest) = t.split_at(t.len() - CHECKSUM_LEN);
 
-    let mut checksum = [t[0]; CHECKSUM_LEN];
-    trits_checksum(body, &mut checksum, curl);
+    let mut checksum = [0 as Trit; CHECKSUM_LEN];
+    trits_checksum(body, &mut checksum, kerl);
 
     if rest != checksum {
         return Some(InvalidChecksum);
@@ -54,47 +56,10 @@ pub enum ChecksumValidationError {
 #[cfg(test)]
 mod test {
     use super::*;
-    use curl_cpu::*;
+    use kerl::*;
     use trytes::*;
     use alloc::vec::Vec;
 
-    #[test]
-    fn checksum_test_1() {
-        let c: Vec<Trit> = "FOXM9MUBX"
-            .chars()
-            .flat_map(char_to_trits)
-            .cloned()
-            .collect();
-        let t: Vec<Trit> = "RVORZ9SIIP9RCYMREUIXXVPQIPHVCNPQ9HZWYKFWYWZRE9JQKG9REPKIASHUUECPSQO9JT9XNMVKWYGVA"
-            .chars()
-            .flat_map(char_to_trits)
-            .cloned()
-            .collect();
-
-        let mut checksum = [0 as Trit; CHECKSUM_LEN];
-        let mut curl = CpuCurl::<Trit>::default();
-        trits_checksum(t.as_slice(), &mut checksum, &mut curl);
-        assert_eq!(c.as_slice(), checksum);
-    }
-
-    #[test]
-    fn checksum_test_2() {
-        let c: Vec<Trit> = "9QTIWOWTY"
-            .chars()
-            .flat_map(char_to_trits)
-            .cloned()
-            .collect();
-        let t: Vec<Trit> = "KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL"
-            .chars()
-            .flat_map(char_to_trits)
-            .cloned()
-            .collect();
-
-        let mut checksum = [0 as Trit; CHECKSUM_LEN];
-        let mut curl = CpuCurl::<Trit>::default();
-        trits_checksum(t.as_slice(), &mut checksum, &mut curl);
-        assert_eq!(c.as_slice(), checksum);
-    }
 
     #[test]
     fn checksum_invalid() {
@@ -104,7 +69,7 @@ mod test {
             .cloned()
             .collect();
 
-        let mut curl = CpuCurl::<Trit>::default();
+        let mut curl = Kerl::default();
         assert_eq!(
             trits_validate_checksum(combined.as_slice(), &mut curl),
             Some(ChecksumValidationError::InvalidChecksum)
@@ -114,11 +79,39 @@ mod test {
     #[test]
     fn checksum_invalid_length() {
         let combined: Vec<Trit> = "KTX".chars().flat_map(char_to_trits).cloned().collect();
-        let mut curl = CpuCurl::<Trit>::default();
+        let mut curl = Kerl::default();
 
         assert_eq!(
             trits_validate_checksum(combined.as_slice(), &mut curl),
             Some(ChecksumValidationError::InvalidLength)
         );
+    }
+
+    #[test]
+    fn checksum_kerl_split() {
+        let c: Vec<Trit> = "ODCNSCYJD"
+            .chars()
+            .flat_map(char_to_trits)
+            .cloned()
+            .collect();
+        let t: Vec<Trit> = "P9UDUZMN9DEXCRQEKLJYSBSBZFCHOBPJSDKMLCCVJDOVOFDWMNBZRIRRZJGINOUMPJBMYYZEGRTIDUABD"
+            .chars()
+            .flat_map(char_to_trits)
+            .cloned()
+            .collect();
+
+        let mut checksum = [0 as Trit; CHECKSUM_LEN];
+        let mut curl = Kerl::default();
+        trits_checksum(t.as_slice(), &mut checksum, &mut curl);
+        assert_eq!(c.as_slice(), checksum);
+    }
+
+    #[test]
+    fn checksum_kerl() {
+        let combined: Vec<Trit> = "EUHMAFIYBYZOXAVQQYRQ9RCNMTYX9KNEZFWXYMQIYPSRZRVDOLXDPUEARYPTWSZCAXJLXRYUUQKSHIJYZICCXCXUHX"
+            .chars().flat_map(char_to_trits).cloned().collect();
+
+        let mut kerl = Kerl::default();
+        assert_eq!(trits_validate_checksum(&combined, &mut kerl), None);
     }
 }
--- sign/src/lib.rs
+++ sign/src/lib.rs
@@ -8,7 +8,11 @@ extern crate alloc;
 extern crate iota_trytes as trytes;
 extern crate iota_tmath as tmath;
 extern crate iota_curl as curl;
+extern crate iota_kerl as kerl;
+
+#[cfg(test)]
 extern crate iota_curl_cpu as curl_cpu;
+
 
 pub mod iss;
 pub mod checksum;
