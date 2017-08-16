f90267cdfd0245a4c924b8465a0879f3414e4cd0
Andreas C. Osowski <andreas@osowski.de>
2017-08-16 15:20:59 +0200

    bindings: fix emscripten build

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -18,6 +18,12 @@ extern crate iota_trytes;
 extern crate iota_curl_cpu;
 extern crate iota_curl;
 
+#[cfg(any(target_os = "emscripten", target_arch = "wasm32"))]
+#[link_args = "-s EXPORTED_FUNCTIONS=['_curl_pair_new','_curl_pair_absorb','_curl_pair_squeeze','_curl_pair_reset','_curl_pair_delete'\
+,'_curl_simple_new','_curl_simple_absorb','_curl_simple_squeeze','_curl_simple_reset','_curl_simple_delete'\
+,'_subseed','_key','_digest_key','_address','_signature','_digest_bundle_signature'\
+]"]
+
 #[cfg(not(test))]
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
--- bindings/src/main.rs
+++ bindings/src/main.rs
@@ -23,27 +23,32 @@ pub mod sign;
 pub mod curl;
 
 #[cfg(any(target_os = "emscripten", target_arch = "wasm32"))]
+extern crate std;
+
+#[cfg(any(target_os = "emscripten", target_arch = "wasm32"))]
 #[link_args = "-s EXPORTED_FUNCTIONS=['_curl_pair_new','_curl_pair_absorb','_curl_pair_squeeze','_curl_pair_reset','_curl_pair_delete'\
 ,'_curl_simple_new','_curl_simple_absorb','_curl_simple_squeeze','_curl_simple_reset','_curl_simple_delete'\
 ,'_subseed','_key','_digest_key','_address','_signature','_digest_bundle_signature'\
 ]"]
 extern "C" {}
 
+
+
 // These functions are used by the compiler, but not
 // for a bare-bones hello world. These are normally
 // provided by libstd.
-#[cfg(not(test))]
+#[cfg(not(any(test, target_os = "emscripten", target_arch = "wasm32")))]
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn rust_eh_personality() {}
 
 // This function may be needed based on the compilation target.
-#[cfg(not(test))]
+#[cfg(not(any(test, target_os = "emscripten", target_arch = "wasm32")))]
 #[lang = "eh_unwind_resume"]
 #[no_mangle]
 pub extern "C" fn rust_eh_unwind_resume() {}
 
-#[cfg(not(test))]
+#[cfg(not(any(test, target_os = "emscripten", target_arch = "wasm32")))]
 #[lang = "panic_fmt"]
 #[no_mangle]
 pub extern "C" fn rust_begin_panic(
@@ -53,6 +58,9 @@ pub extern "C" fn rust_begin_panic(
 ) -> ! {
     unsafe { core::intrinsics::abort() }
 }
+
+
+
 
 #[start]
 pub fn main(_: isize, _: *const *const u8) -> isize {
